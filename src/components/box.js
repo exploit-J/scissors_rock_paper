76e0b058ad677437b6db059c2f73e99b71379203
exploit-j <kkarismajh@gmail.com>
2022-04-26 22:09:13 +0900

    결과에 따른 style 적용

--- src/components/box.js
+++ src/components/box.js
@@ -14,8 +14,18 @@ const Box = (props) => {
     // 위의 경우가 아니라면 props로 전달된 결과를 그대로 쓴다.
     result = props.result;
   }
+
+  let resultClass;
+  if (result == "비겼다!") {
+    resultClass = "tie";
+  } else if (result == "이겼다!") {
+    resultClass = "win";
+  } else if (result == "졌다..") {
+    resultClass = "lose";
+  }
+
   return (
-    <div className={`box ${result}`}>
+    <div className={`box ${resultClass}`}>
       <h1 className="title">{props.name}</h1>
       <h2>{props.item && props.item.name}</h2>
       <img className="img" src={props.item && props.item.img} />
